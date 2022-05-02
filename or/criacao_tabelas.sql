9d84e97127722e1d0184f2c9cdf4d638ce5ab7aa
brunaalvesws <baws@cin.ufpe.br>
2022-05-02 15:21:08 -0300

    criação das tabelas

---
+++ or/criacao_tabelas.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tb_cliente OF tp_cliente;
+
+CREATE TABLE tb_funcionario OF tp_funcionario;
+
+CREATE TABLE tb_medico OF tp_medico;
+
+CREATE TABLE tb_supervisiona OF tp_supervisiona;
+
+CREATE TABLE tb_fornecedor OF tp_fornecedor;
+
+CREATE TABLE tb_compra OF tp_compra;
+
+CREATE TABLE tb_servico OF tp_servico;
+
+CREATE TABLE tb_consulta OF tp_consulta;
+
+
+
