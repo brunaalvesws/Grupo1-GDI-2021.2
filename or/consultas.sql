c9e7b09c849df6b9855d17363d69546bc6c362df
brunaalvesws <baws@cin.ufpe.br>
2022-05-12 18:36:46 -0300

    consultas de cliente ok

--- or/consultas.sql
+++ or/consultas.sql
@@ -1,4 +1,4 @@
 -- Clientes que moram em casa
-SELECT nome FROM tb_cliente WHERE endereco.complemento = "Casa";
+SELECT C.nome FROM tb_cliente C WHERE C.endereco.complemento = 'Casa';
 -- Clientes que são mulheres
-SELECT nome FROM tb_cliente WHERE genero = "F";
+SELECT nome FROM tb_cliente WHERE genero = 'M';
